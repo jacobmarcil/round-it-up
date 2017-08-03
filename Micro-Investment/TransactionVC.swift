fcfee0b7e21006449b74992e51aae6b5fb410e1d
Pierre-Luc Landry <pierre-luc.landry.1@ens.etsmtl.ca>
2017-08-03 12:22:38 -0400

    get liste transactions

--- Micro-Investment/TransactionVC.swift
+++ Micro-Investment/TransactionVC.swift
@@ -7,20 +7,57 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
-class TransactionVC: UIViewController {
+class TransactionVC: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var transactionTableView: UITableView!
+    var refBanque: DatabaseReference!
+    var listeTransactions: [[String:String]] = []
+    var ref:DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        ref = Database.database().reference()
+        refBanque = Database.database().reference().child("banque");
+        numberOfCards()
+        transactionTableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfCards(){
+        if let userID = Auth.auth().currentUser?.uid{
+            refBanque.child(userID).child("1").child("transactions").observeSingleEvent(of: .value, with: { (snapshot) in
+                
+                for transaction in (snapshot.value as? [[String:String]])! {
+                    self.listeTransactions.append(transaction)
+                }
+                
+            }) { (error) in
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.listeTransactions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if let transactionCell = self.transactionTableView.dequeueReusableCell(withIdentifier: "transactionCell", for: indexPath) as? TransactionTableViewCell{
+            
+            for transaction in self.listeTransactions{
+                
+                transactionCell.configureCell(date: transaction["date"]!, description: transaction["description"]!, montant: transaction["montant"]!)
+            }
+        
+            return transactionCell
+        }
+        else{
+            return UITableViewCell()
+        }
     }
     
 }
